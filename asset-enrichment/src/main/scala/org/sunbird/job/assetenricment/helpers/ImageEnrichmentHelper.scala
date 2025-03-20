8a6082d7e7c9d507fb4c0eef8a5a8d8847d36b51
sreerag <sreerag.sajeesh@tarento.com>
2025-03-20 15:49:24 +0530

    Asset enrichment image resolution issue Testing

--- asset-enrichment/src/main/scala/org/sunbird/job/assetenricment/helpers/ImageEnrichmentHelper.scala
+++ asset-enrichment/src/main/scala/org/sunbird/job/assetenricment/helpers/ImageEnrichmentHelper.scala
@@ -61,14 +61,18 @@ trait ImageEnrichmentHelper {
           val dimension = variantValueMap.getOrElse("dimensions", List[Int]()).asInstanceOf[List[Int]]
           val dpi = variantValueMap.getOrElse("dpi", 0).asInstanceOf[Int]
           if (dimension == null || dimension.size != 2) throw new Exception("Content Optimizer Error. Image Resolution/variants is not configured for content optimization.")
-          if (isImageOptimizable(file, dimension(0), dimension(1))) {
+          if (dimension(0) > 5000 && dimension(1) > 5000) {
+            variantsMap.put(resolution, originalURL)
+          } else if (isImageOptimizable(file, dimension(0), dimension(1))) {
             val targetResolution = getOptimalDPI(file, dpi)
             val optimisedFile = optimizeImage(file, targetResolution, dimension(0), dimension(1), resolution)
             if (null != optimisedFile && optimisedFile.exists) {
               val optimisedURLArray = upload(optimisedFile, contentId)(cloudStorageUtil)
               variantsMap.put(resolution, optimisedURLArray(1))
             }
-          } else variantsMap.put(resolution, originalURL)
+          } else {
+            variantsMap.put(resolution, originalURL)
+          }
         })
         case _ => logger.error("ERR_INVALID_FILE_URL", s"Please Provide Valid File Url for identifier: $contentId!")
           throw new Exception(s"Please Provide Valid File Url for identifier : $contentId and URL : $originalURL.")
@@ -100,7 +104,6 @@ trait ImageEnrichmentHelper {
     val height = imageInfo.getImageHeight
     (dimensionX < width && dimensionY < height)
   }
-
   def getOptimalDPI(file: File, dpi: Int): Double = {
     val inputFileName = file.getAbsolutePath
     val imageInfo = new Info(inputFileName, false)
