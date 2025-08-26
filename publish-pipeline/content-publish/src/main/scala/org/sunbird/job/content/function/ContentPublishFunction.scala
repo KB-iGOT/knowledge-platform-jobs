5eb3d5c849c0856a53fa8a9e72eeea06e35dda16
anilkumarkammalapalli <121931293+anilkumarkammalapalli@users.noreply.github.com>
2025-08-26 12:18:42 +0530

    KB-11220 fix: skip notifications for Learning Resource content (#163)

--- publish-pipeline/content-publish/src/main/scala/org/sunbird/job/content/function/ContentPublishFunction.scala
+++ publish-pipeline/content-publish/src/main/scala/org/sunbird/job/content/function/ContentPublishFunction.scala
@@ -99,9 +99,10 @@ class ContentPublishFunction(config: ContentPublishConfig, httpUtil: HttpUtil,
           metrics.incCounter(config.contentPublishSuccessEventCount)
           logger.info("Content publishing completed successfully for : " + data.identifier)
           logger.info("Notification started successfully")
-          val maybeCategory = Option(enrichedObj.metadata.getOrElse("primaryCategory", null))
-          val maybeResourceCategory = Option(enrichedObj.metadata.getOrElse("resourceCategory",null))
-          if (maybeCategory.exists(_.toString.equalsIgnoreCase("Learning Resource")) && !maybeResourceCategory.exists(_.toString.equalsIgnoreCase("Learning Resource"))) {
+          val resourceCategoryOpt = Option(enrichedObj.metadata.get("resourceCategory")).map(_.asInstanceOf[String])
+          val primaryCategoryOpt = Option(enrichedObj.metadata.get("primaryCategory")).map(_.asInstanceOf[String])
+          val categoryToCheck = resourceCategoryOpt.filter(_.nonEmpty).orElse(primaryCategoryOpt).getOrElse("")
+          if (!categoryToCheck.equalsIgnoreCase("Learning Resource")) {
             try {
               logger.info("Node metadata is {}", obj.metadata)
               new NotificationManager(config.notificationUrl, httpUtil).sendNotification(
