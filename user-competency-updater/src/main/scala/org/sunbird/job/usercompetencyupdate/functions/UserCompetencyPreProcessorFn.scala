d13c30487ba58ec2cc290d10e253224aa660b7d7
pathiknandi <pathik.nandi@tarento.com>
2026-03-13 15:15:39 +0530

    KB-13444 added few logs for testing in dev for flink-job

--- user-competency-updater/src/main/scala/org/sunbird/job/usercompetencyupdate/functions/UserCompetencyPreProcessorFn.scala
+++ user-competency-updater/src/main/scala/org/sunbird/job/usercompetencyupdate/functions/UserCompetencyPreProcessorFn.scala
@@ -248,6 +248,7 @@ class UserCompetencyPreProcessorFn(config: UserCompetencyUpdaterConfig, httpUtil
     val courseInfo = getCourseInfo(courseId)(metrics,config,cache,httpUtil)
     val competencies = courseInfo.get(config.competenciesV6Key).asInstanceOf[java.util.List[java.util.Map[String,AnyRef]]].asScala.toList.map(_.asScala.toMap)
     var certificateId = ""
+    logger.info(s"CourseInfo - courseInfo=$courseInfo")
     var acquiredAt = ""
     val certs = enrolment.get(config.issuedCertificatesKey).map(_.asInstanceOf[java.util.List[java.util.Map[String,String]]])
     certs.foreach(l => if (!l.isEmpty) { val c=l.get(0); certificateId=c.getOrDefault(config.identifierKey,""); acquiredAt=c.getOrDefault(config.lastIssuedOnKey,"") })
@@ -257,6 +258,7 @@ class UserCompetencyPreProcessorFn(config: UserCompetencyUpdaterConfig, httpUtil
       val areaId = comp.getOrElse(config.competencyAreaIdentifierKey,"").toString
       val themeId = comp.getOrElse(config.competencyThemeIdentifierKey,"").toString
       val subthemeId = comp.getOrElse(config.competencySubThemeIdentifierKey,"").toString
+      logger.info(s"process competency - areaId=$areaId themeId=$themeId subthemeId=$subthemeId")
       upsertUserCompetencyByContext(userId, areaId, themeId, subthemeId, detailsMap, config.iGOTCourses, config.dbName + "." + config.userCompetencyTable, metrics)
     }
   }
@@ -271,7 +273,7 @@ class UserCompetencyPreProcessorFn(config: UserCompetencyUpdaterConfig, httpUtil
                                              userCompetencyTable: String,
                                              metrics: Metrics
                                            ): Unit = {
-    logger.debug(s"upsertUserCompetencyByContext - reading competency_details for userId=$userId area=$areaId theme=$themeId subtheme=$subthemeId key=$competencyKey")
+    logger.info(s"upsertUserCompetencyByContext - reading competency_details for userId=$userId area=$areaId theme=$themeId subtheme=$subthemeId key=$competencyKey")
     val selectQuery =
       s"SELECT competency_details FROM $userCompetencyTable WHERE user_id='$userId' AND competency_area_id='$areaId' AND competency_theme_id='$themeId' AND competency_subtheme_id='$subthemeId';"
     val existingRows = cassandraUtil.find(selectQuery)
