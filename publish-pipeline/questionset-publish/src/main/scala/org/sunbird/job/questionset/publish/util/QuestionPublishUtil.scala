31916d0b62d17bdc9f0366bf7934b30279b2e3ee
Karthikeyan R <karthikeyanr@TI-MAC-118.local>
2026-03-14 16:22:03 +0530

    KB-13535 using config to avoid ecar generation for questions

--- publish-pipeline/questionset-publish/src/main/scala/org/sunbird/job/questionset/publish/util/QuestionPublishUtil.scala
+++ publish-pipeline/questionset-publish/src/main/scala/org/sunbird/job/questionset/publish/util/QuestionPublishUtil.scala
@@ -2,10 +2,10 @@ package org.sunbird.job.questionset.publish.util
 
 import org.slf4j.LoggerFactory
 import org.sunbird.job.domain.`object`.DefinitionCache
-import org.sunbird.job.publish.config.PublishConfig
 import org.sunbird.job.publish.core.{DefinitionConfig, ExtDataConfig, ObjectData}
 import org.sunbird.job.publish.helpers.EcarPackageType
 import org.sunbird.job.questionset.publish.helpers.QuestionPublisher
+import org.sunbird.job.questionset.task.QuestionSetPublishConfig
 import org.sunbird.job.util.{CassandraUtil, CloudStorageUtil, HttpUtil, Neo4JUtil}
 
 import scala.concurrent.ExecutionContext
@@ -16,7 +16,7 @@ object QuestionPublishUtil extends QuestionPublisher {
 
   private[this] val logger = LoggerFactory.getLogger(classOf[QuestionPublishUtil])
 
-  def publishQuestions(identifier: String, objList: List[ObjectData], pkgVersion: Double)(implicit ec: ExecutionContext, neo4JUtil: Neo4JUtil, cassandraUtil: CassandraUtil, readerConfig: ExtDataConfig, cloudStorageUtil: CloudStorageUtil, definitionCache: DefinitionCache, definitionConfig: DefinitionConfig, config: PublishConfig, httpUtil: HttpUtil): List[ObjectData] = {
+  def publishQuestions(identifier: String, objList: List[ObjectData], pkgVersion: Double)(implicit ec: ExecutionContext, neo4JUtil: Neo4JUtil, cassandraUtil: CassandraUtil, readerConfig: ExtDataConfig, cloudStorageUtil: CloudStorageUtil, definitionCache: DefinitionCache, definitionConfig: DefinitionConfig, config: QuestionSetPublishConfig, httpUtil: HttpUtil): List[ObjectData] = {
     logger.info("QuestionPublishUtil :::: publishing child question for questionset : " + identifier)
     objList.map(qData => {
       logger.info("QuestionPublishUtil :::: publishing child question : " + qData.identifier)
@@ -28,11 +28,18 @@ object QuestionPublishUtil extends QuestionPublisher {
           //create artifact zip locally, upload to cloud and update the artifact URL
           updateArtifactUrl(enrichedObj, EcarPackageType.FULL.toString)(ec, neo4JUtil, cloudStorageUtil, definitionCache, definitionConfig, config, httpUtil)
         } else enrichedObj
-        val objWithEcar = getObjectWithEcar(objWithArtifactUrl, pkgTypes)(ec, neo4JUtil, cloudStorageUtil, config, definitionCache, definitionConfig, httpUtil)
-        logger.info("Ecar generation done for Question: " + objWithEcar.identifier)
-        saveOnSuccess(objWithEcar)(neo4JUtil, cassandraUtil, readerConfig, definitionCache, definitionConfig)
-        logger.info("Question publishing completed successfully for : " + qData.identifier)
-        objWithEcar
+        if (config.enableEcarGeneration) {
+          val objWithEcar = getObjectWithEcar(enrichedObj, pkgTypes)(ec, neo4JUtil, cloudStorageUtil, config, definitionCache, definitionConfig, httpUtil)
+          logger.info("Ecar generation done for Question: " + objWithEcar.identifier)
+          saveOnSuccess(objWithEcar)(neo4JUtil, cassandraUtil, readerConfig, definitionCache, definitionConfig)
+          logger.info("Question publishing completed successfully for : " + qData.identifier)
+          objWithEcar
+        } else {
+          logger.info("Ecar generation skipped for question: " + enrichedObj.identifier)
+          saveOnSuccess(enrichedObj)(neo4JUtil, cassandraUtil, readerConfig, definitionCache, definitionConfig)
+          logger.info("Question publishing completed successfully for : " + qData.identifier)
+          enrichedObj
+        }
       } else {
         saveOnFailure(obj, messages, pkgVersion)(neo4JUtil)
         logger.info("Question publishing failed for : " + qData.identifier)
