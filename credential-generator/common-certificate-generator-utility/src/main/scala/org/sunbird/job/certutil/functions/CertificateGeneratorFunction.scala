0df2f34297183274e356857f4f63bdeba3e087dc
pathiknandi <pathik.nandi@tarento.com>
2026-03-16 12:56:19 +0530

    KB-13444 | BE| Flink job to add competencies of user while external courses

--- credential-generator/common-certificate-generator-utility/src/main/scala/org/sunbird/job/certutil/functions/CertificateGeneratorFunction.scala
+++ credential-generator/common-certificate-generator-utility/src/main/scala/org/sunbird/job/certutil/functions/CertificateGeneratorFunction.scala
@@ -329,6 +329,10 @@ class CertificateGeneratorFunction(config: CertificateGeneratorConfig, httpUtil:
             context.output(config.notifierOutputTag, NotificationMetaData(certMetaData.userId, certMetaData.courseName, issuedOn, certMetaData.courseId,
                certMetaData.templateId, event.partition, event.offset, event.providerName, event.coursePosterImage))
           }
+          val competencyEvent = buildCompetencyAcquiredEvent(certMetaData.userId, certMetaData.courseId)
+          logger.info(s"Firing competency mapping event for user: ${certMetaData.userId} course: ${certMetaData.courseId}")
+          context.output(config.competencyMappingOutputTag, competencyEvent)
+          logger.info("Competency mapping event fired successfully: {}", competencyEvent)
           //context.output(config.userFeedOutputTag, UserFeedMetaData(certMetaData.userId, certMetaData.courseName, issuedOn, certMetaData.courseId, event.partition, event.offset))
         } else {
           metrics.incCounter(config.failedEventCount)
@@ -504,5 +508,19 @@ class CertificateGeneratorFunction(config: CertificateGeneratorConfig, httpUtil:
 
       })
     }
+  }
+
+  /**
+   * Builds a serialized JSON string for the COMPETENCY_ACQUIRED Kafka event.
+   */
+  private def buildCompetencyAcquiredEvent(userId: String, contentId: String): String = {
+    val edata = Map[String, AnyRef](
+      config.eventType   -> config.competencyAcquired,
+      config.userId     -> userId,
+      config.contentId -> contentId,
+      config.batchId     -> "",
+      config.contextType -> config.extCourses
+    )
+    ScalaJsonUtil.serialize(Map[String, AnyRef]("edata" -> edata))
   }
 }
