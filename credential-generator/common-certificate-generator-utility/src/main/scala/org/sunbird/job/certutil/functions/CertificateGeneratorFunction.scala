011cc311f5825cc6311681de43a7926bdcc3dfb0
Arpitha <arpitha.sureshappa@tarento.com>
2026-03-24 21:28:38 +0530

    KB-13609 external course badge award issue fix (#317)
    * KB-13609 courseName updated for notification
    
    * KB-13609 courseName updated for notification
    
    * KB-13609 external course badge award issue fix

--- credential-generator/common-certificate-generator-utility/src/main/scala/org/sunbird/job/certutil/functions/CertificateGeneratorFunction.scala
+++ credential-generator/common-certificate-generator-utility/src/main/scala/org/sunbird/job/certutil/functions/CertificateGeneratorFunction.scala
@@ -325,12 +325,14 @@ class CertificateGeneratorFunction(config: CertificateGeneratorConfig, httpUtil:
           val audit = ScalaJsonUtil.serialize(certificateAuditEvent)
           context.output(config.auditEventOutputTag, audit)
           logger.info("pushAuditEvent: certificate audit event success {}", audit)
+          val badgeAwardEvent = buildBadgeAwardEvent(certMetaData.userId, certMetaData.courseId, "")
+          context.output(config.userBadgeAwardOutputTag, badgeAwardEvent)
+          logger.info("pushed Badge Award Event successfully {}", badgeAwardEvent)
 
           if (config.enableUserNotification) {
             context.output(config.notifierOutputTag, NotificationMetaData(certMetaData.userId, certMetaData.courseName, issuedOn, certMetaData.courseId,
                certMetaData.templateId, event.partition, event.offset, event.providerName, event.coursePosterImage))
           }
-          context.output(config.userBadgeAwardOutputTag, buildBadgeAwardEvent(certMetaData.userId, certMetaData.courseId, ""))
           //context.output(config.userFeedOutputTag, UserFeedMetaData(certMetaData.userId, certMetaData.courseName, issuedOn, certMetaData.courseId, event.partition, event.offset))
         } else {
           metrics.incCounter(config.failedEventCount)
