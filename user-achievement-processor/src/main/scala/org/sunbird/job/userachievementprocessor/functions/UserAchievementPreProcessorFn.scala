c6c39fd1c5b26414e42d5f6e883ee05c9b0ada46
arpithasureshappa <arpitha.sureshappa@tarento.com>
2026-04-08 01:52:53 +0530

    redis count issue fix

--- user-achievement-processor/src/main/scala/org/sunbird/job/userachievementprocessor/functions/UserAchievementPreProcessorFn.scala
+++ user-achievement-processor/src/main/scala/org/sunbird/job/userachievementprocessor/functions/UserAchievementPreProcessorFn.scala
@@ -1064,7 +1064,7 @@ class UserAchievementPreProcessorFn(config: UserBadgeAwardingConfig, httpUtil: H
       val badgeCountRows = cassandraUtil.find(badgeCountQuery.toString)
 
       if (badgeCountRows != null && !badgeCountRows.isEmpty) {
-        val badgeCount = badgeCountRows.get(0)
+        val badgeCount = badgeCountRows.get(0).getLong(0)
         cache.setWithRetryAndTTL(redisKey, badgeCount.toString)
         logger.info(s"Updated badge count cache in Redis (index 2) for userId=$userId, key=$redisKey, count=$badgeCount")
       } else {
